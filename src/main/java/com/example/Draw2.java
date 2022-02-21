51220b1c78d5286489d39a605e5b8a115e2778dc
bitflame <monazzam@protonmail.com>
2022-02-21 16:40:45 -0500

    done and done:-)

--- src/main/java/com/example/Draw2.java
+++ src/main/java/com/example/Draw2.java
@@ -10,30 +10,44 @@ import edu.princeton.cs.algs4.In;
 public class Draw2 {
     public static void main(String[] args) {
         In in = new In("tinyGex2.txt");
-        Graph graph = new Graph(in);
+        int v = in.readInt();
+        int e = in.readInt();
         Draw draw = new Draw();
-        boolean[] marked = new boolean[graph.vertices()];
-        double y = 0.95;
-        List<Double>[] l = (List<Double>[]) new List[graph.vertices()];
-        for (int i = 0; i < graph.vertices(); i++) {
-            draw.square(0.1, y, .02);
-            draw.text(0.1, y, String.valueOf(i));
-            l[i] = new ArrayList<>();
-            l[i].add(0.1);
-            l[i].add(y);
-            y = y - 0.07;
-        }
-        for (int i = 0; i < graph.vertices(); i++) {
-            for (int j : graph.adjacent(i)) {
-                double xCoor = l[i].get(0) + 0.1;
-                double yCoor = l[i].get(1);
+        List<Double>[] l = (List<Double>[]) new List[v];
+        while (!in.isEmpty()) {
+            int x = in.readInt();
+            int y = in.readInt();
+            double xCoor;
+            double yCoor;
+            if (l[x] == null) {
+                xCoor = 0.1;
+                yCoor = (double) (12.0 - x) / 15;
                 draw.square(xCoor, yCoor, .02);
-                draw.text(xCoor, yCoor, String.valueOf(j));
-                draw.line(l[i].get(0), l[i].get(1), xCoor, yCoor);
-                l[i] = new ArrayList<>();
-                l[i].add(xCoor);
-                l[i].add(yCoor);
+                draw.text(xCoor, yCoor, String.valueOf(x));
+                l[x] = new ArrayList<>();
+                l[x].add(xCoor);
+                l[x].add(yCoor);
+                draw.line(xCoor, yCoor, (xCoor + 0.1), yCoor);
+                xCoor = xCoor + 0.1;
+                draw.square(xCoor, yCoor, .02);
+                draw.text(xCoor, yCoor, String.valueOf(y));
+                l[x] = new ArrayList<>();
+                l[x].add(xCoor);
+                l[x].add(yCoor);
+            } else {
+                xCoor = l[x].get(0);
+                yCoor = l[x].get(1);
+                draw.square(xCoor, yCoor, .02);
+                // draw.text(xCoor, yCoor, String.valueOf(x));
+                draw.line(xCoor, yCoor, (xCoor + 0.1), yCoor);
+                xCoor = xCoor + 0.1;
+                draw.square(xCoor, yCoor, .02);
+                draw.text(xCoor, yCoor, String.valueOf(y));
+                l[x] = new ArrayList<>();
+                l[x].add(xCoor);
+                l[x].add(yCoor);
             }
+
         }
     }
 }
